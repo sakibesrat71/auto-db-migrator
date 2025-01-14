4d69cb4af7f139df55339b824594c4a69a6836de
Sakib Esrat <83289622+sakibesrat71@users.noreply.github.com>
2025-01-15 00:23:20 +0600

    Update main.py according to rest of the code base

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 from ui_elems.ChangeLogWindow import ChangeLogWindow
+from ui_elems.EntryPointUI import MotherWindow
+from ui_elems.DbConnectorWindow import DbConnectorWindow
 from PyQt5.QtWidgets import QApplication
+from logics.PrevDbStateChanger import PrevDbStateChanger
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = ChangeLogWindow()
+    window = MotherWindow()
     sys.exit(app.exec_())
+    
+         
